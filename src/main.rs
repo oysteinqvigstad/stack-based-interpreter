7722f74ccad9b9366acadcc86c97b69efb256756
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-04-18 23:34:02 +0200

    added normal mode, separated from repl-mode.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use prog2006_assignment_2::repl_mode;
+use std::env;
+use prog2006_assignment_2::{normal_mode, repl_mode};
 
+/// The `main` function of the program. It checks for command line arguments to determine
+/// the mode of operation. If the command line argument contains "repl", it starts the program
+/// in REPL mode by calling `repl_mode`. If "repl" is not provided as an argument, the program
+/// defaults to normal mode by calling `normal_mode`.
+///
+/// # Examples
+///
+/// To run the program in normal mode:
+/// ```
+/// $ cargo run < filename.txt
+/// ```
+///
+/// To run the program in REPL mode:
+/// ```
+/// $ cargo run -- repl
+/// ```
 fn main() {
-    // TODO: implement read from file
-    // TODO: implement command line way to start REPL
-    repl_mode();
+    let command_line_args: Vec<String> = env::args().collect();
+    if command_line_args.contains(&"repl".to_string()) {
+        repl_mode();
+    } else {
+        normal_mode();
+    }
 }
